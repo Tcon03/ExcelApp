9909cabce5114501779eef63ba07202f0b14179a
T_Con <nguyentruong281203@gmail.com>
2025-09-22 16:21:35 +0700

    Fix UI

---
+++ ExcelAppCR/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ExcelAppCR.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && boolValue)
+            {
+                // Nếu là true , trả về Visible
+                return Visibility.Visible;
+
+
+            }
+            // Ngược lại trả về Collapsed
+            return Visibility.Collapsed;
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
